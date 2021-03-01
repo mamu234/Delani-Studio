0483d49bb13aecf439e7574c38c6f7b9c269a65b
carolyne <pcmaunda@gmail.com>
2021-03-01 20:30:53 +0300

    styled headings and about  page

--- main.js
+++ main.js
@@ -1,35 +1,35 @@
 $(document).ready(function(){
-    $("#dev-icon.png").click(function(){
-      $("#dev-icon.png").slideDown('1500').hide('1000');
-      $("#development").show('1500');
-    });
-    $("#development").click(function(){
-      $("#development").slideUp('1500');
-      $("#dev-icon.png").slideDown('1500');
-    });
+  $("#development-image").click(function(){
+    $("#development-image").slideDown('1500').hide('1000');
+    $("#development").show('1500');
   });
+  $("#development").click(function(){
+    $("#development").slideUp('1500');
+    $("#development-image").slideDown('1500');
+  });
+});
   
-  $(document).ready(function(){
-    $("#design-icon.png").click(function(){
-      $("#design-icon.png").slideDown('1500').hide('1000');
-      $("#design").show('1500');
-    });
-    $("#design").click(function(){
-      $("#design").slideUp('1500');
-      $("#design-icon.png").slideDown('1500');
-    });
+$(document).ready(function(){
+  $("#design-image").click(function(){
+    $("#design-image").slideDown('1500').hide('1000');
+    $("#design").show('1500');
   });
-  
-  $(document).ready(function(){
-    $("#product-image").click(function(){
-      $("#product-image").slideDown('1500').hide('1000');
-      $("#product").show('1500');
-    });
-    $("#product").click(function(){
-      $("#product").slideUp('1500');
-      $("#product-image").slideDown('1500');
-    });
+  $("#design").click(function(){
+    $("#design").slideUp('1500');
+    $("#design-image").slideDown('1500');
   });
+});
+
+$(document).ready(function(){
+  $("#product-image").click(function(){
+    $("#product-image").slideDown('1500').hide('1000');
+    $("#product").show('1500');
+  });
+  $("#product").click(function(){
+    $("#product").slideUp('1500');
+    $("#product-image").slideDown('1500');
+  });
+});
   
   $(document).ready(function(){
     $("#work1").mouseover(function(){
