a10a9964f8cabcd5ba1af7e3e46835f4ab5b7c39
carolyne <pcmaunda@gmail.com>
2021-03-11 08:20:51 +0300

    styled portfolio

--- script.js
+++ script.js
@@ -1,101 +1,79 @@
 $(document).ready(function(){
-  $("#development-image").click(function(){
-    $("#development-image").slideDown('1500').hide('1000');
-    $("#development").show('1500');
+  $(".design").click(function() {
+    $("#design, .design").toggle("slow");
   });
-  $("#development").click(function(){
-    $("#development").slideUp('1500');
-    $("#development-image").slideDown('1500');
+  $("#design").click(function() {
+    $(".design, #design").toggle("slow");
   });
-});
+  $(".development").click(function() {
+    $("#development, .development").toggle("slow");
+  });
+  $("#development").click(function() {
+    $(".development, #development").toggle("slow");
+  });
+  $(".product-mgt").click(function() {
+    $("#product-mgt, .product-mgt").toggle("slow");
+  });
+  $("#product-mgt").click(function() {
+    $(".product-mgt, #product-mgt").toggle("slow");
+  });
+  $('.work1').mouseover(function() {
+    $('.work1-overlay').show();
+  }).mouseout(function() {
+    $('.work1-overlay').hide();
+  });
+  $(".work2").mouseenter(function() {
+    $(".work2-overlay").show();
+  }).mouseleave(function() {
+    $(".work2-overlay").hide();
+  });
+  $(".work3").mouseenter(function() {
+    $(".work3-overlay").show();
+  }).mouseleave(function() {
+    $(".work3-overlay").hide();
+  });
+  $(".work4").mouseenter(function() {
+    $(".work4-overlay").show();
+  }).mouseleave(function() {
+    $(".work4-overlay").hide();
+  });
+  $(".work5").mouseenter(function() {
+    $(".work5-overlay").show();
+  }).mouseleave(function() {
+    $(".work5-overlay").hide();
+  });
+  $(".work6").mouseenter(function() {
+    $(".work6-overlay").show();
+  }).mouseleave(function() {
+    $(".work6-overlay").hide();
+  });
+  $(".work7").mouseenter(function() {
+    $(".work7-overlay").show();
+  }).mouseleave(function() {
+    $(".work7-overlay").hide();
+  });
+  $(".work8").mouseenter(function() {
+    $(".work8-overlay").show();
+  }).mouseleave(function() {
+    $(".work8-overlay").hide();
+  });
 
-$(document).ready(function(){
-  $("#design-image").click(function(){
-    $("#design-image").slideDown('1500').hide('1000');
-    $("#design").show('1500');
-  });
-  $("#design").click(function(){
-    $("#design").slideUp('1500');
-    $("#design-image").slideDown('1500');
-  });
-});
 
-$(document).ready(function(){
-  $("#product-image").click(function(){
-    $("#product-image").slideDown('1500').hide('1000');
-    $("#product").show('1500');
-  });
-  $("#product").click(function(){
-    $("#product").slideUp('1500');
-    $("#product-image").slideDown('1500');
-  });
-});
+ $("form#form1").on('submit',function(event){
+           event.preventDefault();
+           var name = $("input#name").val();
+           var email = $("input#email").val();
+           var message = $("textarea#message").val();
 
-$(document).ready(function(){
-  $("#work1").mouseover(function(){
-    $("#overlay").show();
-  }).mouseout(function(){
-    $("#overlay").hide();
-  });
-});
-$(document).ready(function(){
-  $("#work2").mouseover(function(){
-    $("#overlay2").show();
-  }).mouseout(function(){
-    $("#overlay2").hide();
-  });
-});
-$(document).ready(function(){
-  $("#work3").mouseover(function(){
-    $("#overlay3").show();
-  }).mouseout(function(){
-    $("#overlay3").hide();
-  });
-});
-$(document).ready(function(){
-  $("#work4").mouseover(function(){
-    $("#overlay4").show();
-  }).mouseout(function(){
-    $("#overlay4").hide();
-  });
-});
-
-$(document).ready(function(){
-  $("#work5").mouseover(function(){
-    $("#overlay5").show();
-  }).mouseout(function(){
-    $("#overlay5").hide();
-  });
-  $("#work6").mouseover(function(){
-    $("#overlay6").show();
-  }).mouseout(function(){
-    $("#overlay6").hide();
-  });
-  $("#work7").mouseover(function(){
-    $("#overlay7").show();
-  }).mouseout(function(){
-    $("#overlay7").hide();
-  });
-  $("#work8").mouseover(function(){
-    $("#overlay8").show();
-  }).mouseout(function(){
-    $("#overlay8").hide();
-  });
-});
-
-$(document).ready(function(){
-  $("form#form34A").submit(function(event){
-    event.preventDefault();
-    var name = $("input#MERGE1").val();
-    var email = $("input#MERGE0").val();
-    var message = $("textarea#comment").val();
-    if ($("input#MERGE1").val() && $("input#MERGE0").val()){
-      alert (name + ", we have received your message. Thank you for reaching out to us.");
-    }
-    else {
-      alert("Please enter your name and email!");
-    }
-    
-  });
+           if ($("input#name").val() && $("input#email").val() && $("input#message").val() ){
+               alert ("Hello " + name + ", Thank you for reaching out, we'll get back to you shortly...");
+           }
+           else if($("input#name").val() && $("input#email").val()){
+               alert("Thank you for reaching ou to us. If you wish to leave a message please type in on the message section..");
+           }
+          else{
+            alert("Please provide correct name or email")
+          }
+       });
 
 });
