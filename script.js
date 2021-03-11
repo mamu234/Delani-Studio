48b99ab5fade45289d9a197bdab201f6e8b44bf7
carolyne <pcmaunda@gmail.com>
2021-03-11 08:53:10 +0300

    modified portfolio section

--- script.js
+++ script.js
@@ -17,46 +17,8 @@ $(document).ready(function(){
   $("#product-mgt").click(function() {
     $(".product-mgt, #product-mgt").toggle("slow");
   });
-  $('.work1').mouseover(function() {
-    $('.work1-overlay').show();
-  }).mouseout(function() {
-    $('.work1-overlay').hide();
-  });
-  $(".work2").mouseenter(function() {
-    $(".work2-overlay").show();
-  }).mouseleave(function() {
-    $(".work2-overlay").hide();
-  });
-  $(".work3").mouseenter(function() {
-    $(".work3-overlay").show();
-  }).mouseleave(function() {
-    $(".work3-overlay").hide();
-  });
-  $(".work4").mouseenter(function() {
-    $(".work4-overlay").show();
-  }).mouseleave(function() {
-    $(".work4-overlay").hide();
-  });
-  $(".work5").mouseenter(function() {
-    $(".work5-overlay").show();
-  }).mouseleave(function() {
-    $(".work5-overlay").hide();
-  });
-  $(".work6").mouseenter(function() {
-    $(".work6-overlay").show();
-  }).mouseleave(function() {
-    $(".work6-overlay").hide();
-  });
-  $(".work7").mouseenter(function() {
-    $(".work7-overlay").show();
-  }).mouseleave(function() {
-    $(".work7-overlay").hide();
-  });
-  $(".work8").mouseenter(function() {
-    $(".work8-overlay").show();
-  }).mouseleave(function() {
-    $(".work8-overlay").hide();
-  });
+  
+  
 
 
  $("form#form1").on('submit',function(event){
@@ -76,4 +38,4 @@ $(document).ready(function(){
           }
        });
 
-});
+      })
