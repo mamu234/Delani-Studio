aad885cf9dcc35733622d370cf94e387e3d49dca
carolyne <pcmaunda@gmail.com>
2021-03-13 15:43:45 +0300

    styled what we do section

--- script.js
+++ script.js
@@ -1,41 +1,74 @@
 $(document).ready(function(){
-  $(".design").click(function() {
-    $("#design, .design").toggle("slow");
+  $("#development-image").click(function(){
+    $("#development-image").slideDown('1500').hide('1000');
+    $("#development").show('1500');
   });
-  $("#design").click(function() {
-    $(".design, #design").toggle("slow");
+  $("#development").click(function(){
+    $("#development").slideUp('1500');
+    $("#development-image").slideDown('1500');
   });
-  $(".development").click(function() {
-    $("#development, .development").toggle("slow");
+});
+
+$(document).ready(function(){
+  $("#design-image").click(function(){
+    $("#design-image").slideDown('1500').hide('1000');
+    $("#design").show('1500');
   });
-  $("#development").click(function() {
-    $(".development, #development").toggle("slow");
+  $("#design").click(function(){
+    $("#design").slideUp('1500');
+    $("#design-image").slideDown('1500');
   });
-  $(".product-mgt").click(function() {
-    $("#product-mgt, .product-mgt").toggle("slow");
+});
+
+$(document).ready(function(){
+  $("#product-image").click(function(){
+    $("#product-image").slideDown('1500').hide('1000');
+    $("#product").show('1500');
   });
-  $("#product-mgt").click(function() {
-    $(".product-mgt, #product-mgt").toggle("slow");
+  $("#product").click(function(){
+    $("#product").slideUp('1500');
+    $("#product-image").slideDown('1500');
   });
+});
   
   
 
 
- $("form#form1").on('submit',function(event){
-           event.preventDefault();
-           var name = $("input#name").val();
-           var email = $("input#email").val();
-           var message = $("textarea#message").val();
+const form = document.getElementById('mc-embedded-subscribe-form');
+const formName = document.getElementById('mce-MMERGE6');
+const formEmail= document.getElementById('mce-EMAIL');
+const formMessage = document.getElementById('mce-MMERGE2');
 
-           if ($("input#name").val() && $("input#email").val() && $("input#message").val() ){
-               alert ("Hello " + name + ", Thank you for reaching out, we'll get back to you shortly...");
-           }
-           else if($("input#name").val() && $("input#email").val()){
-               alert("Thank you for reaching ou to us. If you wish to leave a message please type in on the message section..");
-           }
-          else{
-            alert("Please provide correct name or email")
-          }
-       });
 
-      })
+form.addEventListener("submit", e=>{
+    e.preventDefault();
+    console.log(formName.value + formEmail.value + formMessage.value);
+    validateForm()
+})
+
+
+function validateForm(){
+    if(formName.value === ""){ 
+        alert("Please Enter Your Name");
+    }else if(formEmail.value === ""){
+        alert("Please Enter Your Email");
+    }else if(formMessage.value === ""){
+    alert("Please Enter Your Message");  
+    }else{
+        success();  
+    }
+}
+
+function success(){
+    alert("Thank you for contacting us " +(formName.value).toUpperCase() + " Your Message has been received" );
+    clearFields();
+}
+
+
+function clearFields(){
+    formName.value ='';
+    formEmail.value ='';
+    formMessage.value='';
+};
+
+  
